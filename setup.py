ef2cc64316718a3badb13cea54b9455b0eb89f15
johSchm <johann853@outlook.de>
2019-05-02 08:30:56 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='RRT',
+   version='1.0',
+   description='Rapidly Exploring Random Tree Algorithm',
+   author='J. Schmidt',
+   author_email='',
+   packages=['src'],
+   install_requires=['sys', 'random', 'math', 'pygame', 'enum'],
+)
